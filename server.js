419517eb57a1d9a39181cbaec8d434850c25aa88
futurebound <mitchellm1618@gmail.com>
2025-01-03 09:38:19 -0800

    feat: fs, url, path to load HTML files for pages

--- server.js
+++ server.js
@@ -6,27 +6,42 @@
 */
 
 import dotenv from 'dotenv'
-
 import http from 'http'
+import fs from 'fs/promises'
+import url from 'url'
+import path from 'path'
 import { createServer } from 'node:http'
+import { resourceLimits } from 'node:worker_threads'
 
-dotenv.config()
-
+/* Initialize server */
+dotenv.config() // MUST come before usage
 const PORT = process.env.PORT
 
-const server = http.createServer((req, res) => {
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+console.log(`filename: ${__filename}\n dirname: ${__dirname}`)
+
+const server = http.createServer(async (req, res) => {
   try {
     if (req.method === 'GET') {
+      let filePath
       if (req.url === '/') {
-        res.writeHead(200, { 'Content-Type': 'text/html' })
-        res.end('<h1>Home</h1>')
+        filePath = path.join(__dirname, 'public', 'index.html')
+        // res.writeHead(200, { 'Content-Type': 'text/html' })
+        // res.end('<h1>Home</h1>')
       } else if (req.url === '/about') {
-        res.writeHead(200, { 'Content-Type': 'text/html' })
-        res.end('<h1>About</h1>')
+        filePath = path.join(__dirname, 'public', 'about.html')
+        // res.writeHead(200, { 'Content-Type': 'text/html' })
+        // res.end('<h1>About</h1>')
       } else {
         res.writeHead(404, { 'Content-Type': 'text/html' })
         res.end('<h1>404: Not Found</h1>')
       }
+
+      const data = await fs.readFile(filePath)
+      res.setHeader('Content-Type', 'text/html')
+      res.write(data)
+      res.end()
     } else {
       throw new Error('Method not allowed')
     }
